1ce943d2ccb5e76fd2356abde7c95681c7335a92
dxdmeow <118554589+dxdmeow@users.noreply.github.com>
2024-10-02 08:55:20 -0700

    pathTransition demo

--- src/main/java/com/example/demoapppractise1/HelloApplication.java
+++ src/main/java/com/example/demoapppractise1/HelloApplication.java
@@ -1,20 +1,58 @@
 package com.example.demoapppractise1;
 
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import org.controlsfx.control.action.Action;
+import org.controlsfx.control.spreadsheet.Grid;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        StackPane stackPane=new StackPane();
+        Circle circle=new Circle(20);
+        Circle circle1=new Circle(100);
+        circle1.setStroke(Color.GREEN);
+        circle1.setFill(Color.TRANSPARENT);
+        stackPane.getChildren().addAll(circle,circle1);
+        PathTransition pathTransition=new PathTransition();
+        pathTransition.setDuration(Duration.millis(1000));
+        pathTransition.setPath(circle1);
+        pathTransition.setNode(circle);
+        pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        pathTransition.setCycleCount(Timeline.INDEFINITE);
+        pathTransition.setAutoReverse(true);
+        pathTransition.play();
+        circle.setOnMousePressed(event -> pathTransition.pause());
+        circle.setOnMouseReleased(event -> pathTransition.play());
+        Scene scene=new Scene(stackPane,300,400);
         stage.setScene(scene);
         stage.show();
+    }
+    private void calculate(String itemPrice, String installmentMonths, String installmentAmount) {
+        Double price=Double.parseDouble(itemPrice);
+        Double insMonths=Double.parseDouble(installmentMonths);
+        Double insAmount=Double.parseDouble(installmentAmount);
+        double paymentEachMonths,totalPayment;
+        paymentEachMonths=0;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/demoapppractise1/Storage.java
@@ -0,0 +1,98 @@
+package com.example.demoapppractise1;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class Storage{
+    //        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
+    //Pane pane=new HBox();
+//        pane.setPadding(new Insets(5,5,5,5));
+//        Image image=new Image("file:D:\\ProjectsBush\\project1\\demoAppPractise1\\image1.jpg");
+    //Image image1=new Image("https://www.bing.com/images/search?view=detailV2&ccid=FEqv7YYM&id=A434844EF79707671778B4F74B55C55DDC925499&thid=OIP.FEqv7YYMNjXtrVYqo7HHzAHaE7&mediaurl=https%3A%2F%2Fwww.publicdomainpictures.net%2Fpictures%2F170000%2Fvelka%2Flandschaft-1463581037RbE.jpg&cdnurl=https%3A%2F%2Fth.bing.com%2Fth%2Fid%2FR.144aafed860c3635edad562aa3b1c7cc%3Frik%3DmVSS3F3FVUv3tA%26pid%3DImgRaw%26r%3D0&exph=1279&expw=1920&q=free+stock+images&simid=608031116455722482&FORM=IRPRST&ck=4E91CA6F62B3FDD9E0C7CC1DDA60A559&selectedIndex=0&itb=0&cw=1912&ch=954&ajaxhist=0&ajaxserp=0");
+//        System.out.println(image.getUrl());
+//        ImageView imageView=new ImageView(image);
+//        imageView.setFitHeight(400);
+//        imageView.setFitWidth(300);
+//        ImageView imageView1=new ImageView(image1.getUrl());
+//        imageView1.setFitWidth(300);
+//        imageView1.setFitHeight(400);
+    //imageView1.setRotate(90);
+    //pane.getChildren().add(imageView).add(imageView1);
+//        pane.getChildren().addAll(imageView,imageView1);
+    //Scene scene=new Scene(pane,1200,600);
+    //
+    //
+//        FlowPane flowPane=new FlowPane();
+//        flowPane.setPadding(new Insets(11,12,13,14));
+//        flowPane.setOrientation(Orientation.VERTICAL);
+//        flowPane.setHgap(5);
+//        flowPane.setVgap(5);
+//        Label firstName=new Label("First Name : ");
+//        Label lastName=new Label("Last Name : ");
+//        TextField firstNameT=new TextField();
+//        TextField lastNameT=new TextField();
+//        flowPane.getChildren().addAll(firstName,firstNameT,lastName,lastNameT);
+//        Scene scene=new Scene(flowPane,200,250);
+    //
+    //
+//        GridPane gridPane=new GridPane();
+//        gridPane.setAlignment(Pos.CENTER);
+//        gridPane.setPadding(new Insets(11,12,13,14));
+//        gridPane.setHgap(5.5);
+//        gridPane.setVgap(5.5);
+//        Label firstName=new Label("First Name");
+//        Label lastName=new Label("Last Name");
+//        TextField firstNameT=new TextField();
+//        TextField lastNameT=new TextField();
+//        gridPane.add(firstName,0,0);
+//        gridPane.add(firstNameT,0,1);
+//        gridPane.add(lastName,0,2);
+//        gridPane.add(lastNameT,0,3);
+//        Line line = new Line(20,40,20,80);
+//        line.startXProperty().bind(gridPane.widthProperty().subtract(10));
+//        line.startYProperty().bind(gridPane.heightProperty().subtract(10));
+//
+//        line.setStrokeWidth(5);
+//        line.setStroke(Color.GREEN);
+//        gridPane.getChildren();
+    //
+    //
+//        Scene scene=new Scene(gridPane,200,250);
+//        stage.setTitle("Application");
+//        stage.setScene(scene);
+//        stage.show();
+}
+class secondStorage{
+//    Button calculateButton=new Button("Calculate");
+//    Label itemID=new Label("Item ID: ");
+//    Label ItemPrice=new Label("Item Price: ");
+//    Label InstallmentMonths=new Label("Installment Months: ");
+//    Label InstallAmount=new Label("Install Amount: ");
+//    Label PaymentEachMonths=new Label("Payment Each Months: ");
+//    Label TotalPayment=new Label("Total Payment: ");
+//    TextField itemId=new TextField();
+//    TextField itemPrice=new TextField();
+//    TextField installmentMonths=new TextField();
+//    TextField installmentAmount=new TextField();
+//    TextField paymentEachMonths=new TextField();
+//    TextField totalPayment=new TextField();
+//        calculateButton.setOnAction(actionEvent -> calculate(itemPrice.getText(),installmentMonths.getText(),installmentAmount.getText()));
+//    HBox hBox=new HBox();
+//        hBox.setSpacing(30);
+//        hBox.setPadding(new Insets(20,20,20,20));
+//    VBox container=new VBox();
+//        container.setSpacing(30);
+//        container.setPadding(new Insets(20,20,20,20));
+//        container.getChildren().addAll(itemID,itemId,ItemPrice,itemPrice,InstallmentMonths,installmentMonths,InstallAmount,installmentAmount,PaymentEachMonths,paymentEachMonths,TotalPayment,totalPayment,calculateButton);
+//    Scene scene=new Scene(container,1200,800);
+//        stage.setTitle("Simple Click");
+//        stage.setScene(scene);
+}
